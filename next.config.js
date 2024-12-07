c61ba7c6ebedf8cb721b32406385fead7577b57d
Anara Zhunusova <anarazhunusova@Anaras-MacBook-Pro-2.local>
2024-12-07 23:06:06 +0100

    updated sanity cms

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['cdn.sanity.io'], // Allow Next.js to optimize images from Sanity's CDN
+  },
 }
 
 module.exports = nextConfig
